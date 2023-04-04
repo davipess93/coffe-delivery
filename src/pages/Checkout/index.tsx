1a3e42166b849d968ccee7e284b86562faf128c4
Davi Pessoa <davialvespessoa@gmail.com>
2023-04-03 23:30:40 -0300

    Titulo do formulario do checkout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,10 +1,29 @@
-import { CheckoutContainer, CheckoutTitle } from './styles'
+import { MapPinLine } from 'phosphor-react'
+import {
+  CheckoutContainer,
+  CheckoutFormContainer,
+  CheckoutFormHeader,
+  CheckoutFormSubtitle,
+  CheckoutFormTitle,
+  CheckoutTitle,
+} from './styles'
 
 export function Checkout() {
   return (
     <CheckoutContainer>
       <div>
         <CheckoutTitle>Complete seu pedido</CheckoutTitle>
+        <CheckoutFormContainer>
+          <CheckoutFormHeader>
+            <MapPinLine size={22} />
+            <div>
+              <CheckoutFormTitle>Endereço de Entrega</CheckoutFormTitle>
+              <CheckoutFormSubtitle>
+                Informe o endereço onde deseja receber seu pedido
+              </CheckoutFormSubtitle>
+            </div>
+          </CheckoutFormHeader>
+        </CheckoutFormContainer>
       </div>
       <div>
         <CheckoutTitle>Cafés selecionados</CheckoutTitle>
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -10,3 +10,33 @@ export const CheckoutContainer = styled.div`
 export const CheckoutTitle = styled.h1`
   font-size: 18px;
 `
+
+export const CheckoutFormContainer = styled.div`
+  margin-top: 20px;
+  padding: 40px;
+  border-radius: 6px;
+  background-color: ${(props) => props.theme.card};
+`
+
+export const CheckoutFormHeader = styled.div`
+  display: flex;
+  gap: 8px;
+
+  svg {
+    color: ${(props) => props.theme.darkYellow};
+  }
+`
+
+export const CheckoutFormTitle = styled.p`
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 130%;
+  color: ${(props) => props.theme.subtitle};
+`
+
+export const CheckoutFormSubtitle = styled.p`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 130%;
+  color: ${(props) => props.theme.text};
+`
